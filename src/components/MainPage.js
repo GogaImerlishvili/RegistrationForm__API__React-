0e8a19d967fc5e3dea4b935322975ebe344f7328
Goga <gogaimerlishvili1@gmail.com>
2022-07-28 02:59:40 +0400

    added mainpage context

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import RocketMan from "../img/rocketman.png";
 import classes from "./MainPage.module.css";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
+  const nextPage = () => {
+    navigate("/page1");
+  };
   return (
     <div className={classes["main_container"]}>
-      <h1>Hello</h1>
+      <div className={classes.title}>
+        <h1>welcome rocketeer!</h1>
+        <div className={classes["start-button"]}>
+          <button type="button" onClick={nextPage}>
+            Start Questionnaire
+          </button>
+        </div>
+        <div className={classes["submitted-button"]}>
+          <button>submitted button</button>
+        </div>
+        <div className={classes.img}>
+          <img src={RocketMan} alt="rocket man" />
+        </div>
+      </div>
     </div>
   );
 };

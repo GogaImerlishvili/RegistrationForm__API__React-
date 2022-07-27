4d24c2cac6ee8a690ea31bea3499697ec086fba2
Goga <gogaimerlishvili1@gmail.com>
2022-07-28 01:13:24 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage.js";
+import Page1 from "./components/Page1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/page1" element={<Page1 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./MainPage.module.css";
+
+const MainPage = () => {
+  return (
+    <div className={classes["main_container"]}>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default MainPage;

dddaea757ace967124bc99e568599fb567599f56
Goga <gogaimerlishvili1@gmail.com>
2022-07-28 18:26:11 +0400

    added path of page2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<MainPage />} />
         <Route exact path="/page1" element={<Page1 />} />
+        <Route exact path="/page2" element={<Page2 />} />
       </Routes>
     </div>
   );
